660dbb80cc28552c3cb5df80b545a59b5825ab16
KlyukinSA <ctepa.kl@mail.ru>
2024-05-16 13:43:33 +0300

    error on tmp/

--- FileTapeFactory.cpp
+++ FileTapeFactory.cpp
@@ -1,5 +1,8 @@
 #include "FileTapeFactory.hpp"
 
+#include <stdexcept>
+#include <utility>
+
 FileTapeFactory::FileTapeFactory(const FileTapeConfig& config, const std::string& directory)
     : i(0)
     , config(config)
@@ -8,7 +11,11 @@ FileTapeFactory::FileTapeFactory(const FileTapeConfig& config, const std::string
 
 FileTape FileTapeFactory::create() {
     i++;
-    return FileTape{std::fstream{getTempTapeFileName(i), std::ios::in | std::ios::out | std::ios::trunc}, config};
+    std::fstream file{getTempTapeFileName(i), std::ios::in | std::ios::out | std::ios::trunc};
+    if (!file.is_open()) {
+        throw std::runtime_error{std::string{"cant open "} + getTempTapeFileName(i) + ". check directory existance"};
+    }
+    return FileTape{std::move(file), config};
 }
 
 std::string FileTapeFactory::getTempTapeFileName(int id) {
--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 `test/TempTapeTest.cpp` содержит unit test для TempTape.
 
 ```sh
+mkdir tmp
+echo "readDelay=1 // microseconds" > test/config.txt
+
 make && ./prog test/input2.txt test/output.txt
 make && rm test/output.txt && ./prog test/input1.txt test/output.txt && cat test/output.txt
 make clean && make && rm test/output.txt && ./prog test/input1.txt test/output.txt && cat test/output.txt
+
 g++ TempTapeTest.cpp && ./a.out
 g++ FileTapeTest.cpp ../FileTape.cpp && ./a.out
 g++ config.cpp ../FileTapeConfig.cpp && ./a.out
